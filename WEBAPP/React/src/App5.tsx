b8ef6375b825a260829b030f82a5c634b2dcedb7
Alperen <alperen@example.com>
2025-11-18 10:39:19 +0100

    Aufgabe 6 Started & Finished Passing Functions

--- WEBAPP/React/src/App5.tsx
+++ WEBAPP/React/src/App5.tsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import Cards from "./components/5 Aufgabe Komponenten State/Cards";
 import Counter from "./components/5 Aufgabe Komponenten State/Counter";
 import LightSwtich from "./components/5 Aufgabe Komponenten State/LightSwitch";
+import Button from "./components/6 Aufgabe Passing Functions/Button";
 
 function App() {
+  const [isOn, setIsOn] = useState(false);
+  const [sayHello, setSayHello] = useState(false);
   return (
     <div>
-      <h1>Aufgabe 5 Komponenten State</h1>
+      <h1 className="font-bold">Aufgabe 5 Komponenten State</h1>
       <br />
       <div className="grid grid-cols-2 w-80 gap-4">
         <Cards name="Jason Smith" description="Bagger Fahrer" />
@@ -37,6 +41,31 @@ function App() {
         <br />
         <br />
       </div>
+
+      <h1 className="font-bold">Aufgabe 6 - Passing Functions</h1>
+
+      {isOn && <div>It is on</div>}
+      {sayHello && <div>Hello you!</div>}
+      <br />
+
+      <div className="grid grid-rows-2 cols-row gap-4 px-10">
+        <Button
+          title={isOn ? "Turn off" : "Turn on"}
+          onPress={() => {
+            setIsOn(!isOn);
+          }}
+        />
+        {!sayHello && (
+          <Button
+            title="Say Hello!"
+            onPress={() => {
+              setSayHello(!sayHello);
+            }}
+          />
+        )}
+      </div>
+      <br />
+      <br />
     </div>
   );
 }
---
+++ WEBAPP/React/src/components/6 Aufgabe Passing Functions/Button.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+type Props = {
+  title: string;
+  onPress: () => void;
+};
+
+export default function Button({ title, onPress }: Props) {
+  return (
+    <div
+      className="bg-amber-300 px-4 py-4 w-25 text-amber-50 rounded hover:bg-amber-500 hover:cursor-pointer"
+      onClick={onPress}
+    >
+      {title}
+    </div>
+  );
+}
