0885bb81142915b8bbc3f5cdb54fa92ffdf86ce4
Alperen <alperen@example.com>
2025-11-18 11:16:27 +0100

    Started Aufgabe 7 Use Effect

---
+++ WEBAPP/React/src/App7.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import Card from "./components/7 Aufgabe UseEffect/Todo";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-bold">Aufgabe 7 Todo</h1>
+    </div>
+  );
+}
+
+export default App;
